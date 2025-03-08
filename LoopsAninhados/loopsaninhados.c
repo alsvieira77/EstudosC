a2359487c01f21a5a5718e6428e76e0b8073fcb4
alsvieira77 <andrevieira.cwb@gmail.com>
2025-03-08 15:54:38 -0300

    Loops Aninhados

---
+++ LoopsAninhados/loopsaninhados.c
@@ -0,0 +1,49 @@
+// É um loop dentro de outro loop
+// Usados para multiplas dimensões de dados
+
+// resolução de problemas mais complexos
+// iterações multiplas e hierarquicas
+// aplicações em matrizes e tabelas
+
+
+// CUIDADOS
+// uso pode causar ineficiencia
+// requer organização dos codigos
+// podem existir alternativas mais eficientes em uso
+
+// APLICAÇÕES
+// Algoritimos de força bruta
+// MAtrizes
+// Criptografia e segurança
+
+// DESAFIOS
+// Compreensao da logica e estrutura
+// Erro comum: acesso dos indices fora do limite
+
+// Estrutura:
+// nome da estrutura
+// (condição)
+// Código
+
+/*
+    estrutura (condição) {
+        estrutura (condição){
+            CÓDIGO
+        }
+    }
+*/
+// Cada execução do loop externo executará completamente o loop interno.
+
+// Condição é booleana
+// Estrutura interna e externa
+
+// DESEMPENHO
+// Computacionalmente caro
+/*
+Crescimento exponencial:
+- O(n^2) Para loops de "i" e "j"
+- O(n^3) Para loops de "i", "j" e "k"
+-...n
+*/
+
+
